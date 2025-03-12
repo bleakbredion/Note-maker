97c6f2be4b436a6884f3a14133db92b1ab4938b3
Rostislav Zyablov <rostza007@gmail.com>
2025-03-12 22:02:24 +0700

    Removed unworked files

--- GLOBAL convert.py
+++
@@ -1,5 +0,0 @@
-INFILENAME =
-RATE = 44100  # Частота дискретизации
-DURATION = 20 # Длительность записи в секундах
-CHANNELS = 2
-DATATYPE = 'int16'
--- GLOBAL recording.py
+++
@@ -1,5 +0,0 @@
-OUTFILENAME = outfile
-RATE = 44100  # Частота дискретизации
-CHANNELS = 2
-DATATYPE = 'int16'
-DURATION = 20 # Длительность записи в секундах
