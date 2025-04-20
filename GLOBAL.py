15263db60d57a12c6d4f66e3c043ce6a472535ad
Rostislav Zyablov <rostza007@gmail.com>
2025-04-21 05:25:37 +0700

    добавлен файл-координатор-путей GLOBAL.py

---
+++ GLOBAL.py
@@ -0,0 +1,9 @@
+RECOGNIZED_TEXTS = ["/home/rostislav/python/speech recognition/Распознанные тексты/распознанный biology_test1.wav.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/Яна монолог.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/Яна разделенный монолог.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/Яна монолог whisper-large-v3-turbo-russian try 1.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/Яна монолог whisper-large-v3-turbo-russian try 2.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/Яна монолог whisper-small try 1.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/распознанный biology_test1.wav.txt", "/home/rostislav/python/speech recognition/Распознанные тексты/распознаный текст Яна монолог.txt"]
+
+PYTHON_RECORDING_AND_RECOGNITION = "/home/rostislav/.pyenv/versions/3.11.0/envs/note_maker/bin/python"
+PYTHON_MAKE_PARAGRAPHS = "/home/rostislav/.pyenv/versions/deep_pavlov/bin/python"
+
+# Пути к файлам
+DRAFT_TEXT_PATH = "/home/rostislav/python/note maker/Useful files/texts/draft_text.txt"
+STRUCTURED_TEXT_PATH = "/home/rostislav/python/note maker/Useful files/texts/structured_text.txt"
+AUDIO_OUTPUT_PATH = "/home/rostislav/python/note maker/Useful files/output.wav"
